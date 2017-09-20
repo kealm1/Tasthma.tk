a331cd76b92cefb9d19e67a68f731610f13d9c06
kealm1 <31068571+kealm1@users.noreply.github.com>
2017-09-21 02:44:16 +1000

    Add files via upload

--- Tasthma-web/scripts/dbController.php
+++ Tasthma-web/scripts/dbController.php
@@ -3,46 +3,59 @@
  * Created by PhpStorm.
  * User: Jean-cauce
  * Date: 8/9/17
- * Time: 6:06 PM
+ * Time:
+ *
+ * V1 handle all the post data from website, perform insert, update, delete according to toe post data
+ *
+ * and it will respond to website with a code and related msgs, So website can response according.
+ * 0 error,
+ * 1 sub service 1,
+ * 2 sub service 2,
+ * 3 sub both,
+ * 4 update to 1,
+ * 5 update to 2,
+ * 6 update to both,
+ * 7 unsub
  */
 error_reporting(0);
-require 'connectToDB.php';
-require 'sendSMS.php';
+require 'connectToDB.php'; //connect to database
+require 'sendSMS.php'; // send msg to user after processing
 
-$func = $_POST['function'];
-$postcode = $_POST['postcode'];
-$phone = $_POST['phoneNumber'];
-$serviceType = $_POST['serviceType'];
+$func = $_POST['function']; //insert, update, delete
+$postcode = $_POST['postcode']; //data postcode
+$phone = $_POST['phoneNumber']; //data mobile number
+$serviceType = $_POST['serviceType']; //user preference 0 unsub, 1 service 1, 2 service 2, 3 both service
 
 
-$checkPhoneFromArea = $db->query("SELECT * FROM areaSubList WHERE phoneNumber = '{$phone}'");
-$checkPhoneFromDaily = $db->query("SELECT * FROM dailySubList WHERE phoneNumber = '{$phone}'");
-$checkPostcode = $db->query("SELECT * FROM suburbs WHERE postcode = '{$postcode}'");
+$checkPhoneFromArea = $db->query("SELECT * FROM areaSubList WHERE phoneNumber = '{$phone}'"); //find record in table of service 1 list
+$checkPhoneFromDaily = $db->query("SELECT * FROM dailySubList WHERE phoneNumber = '{$phone}'"); //find record in table of service 2 list
+$checkPostcode = $db->query("SELECT * FROM suburbs WHERE postcode = '{$postcode}'"); //find post code info in table
 
-$dQuery = "DELETE FROM dailySubList WHERE phoneNumber = '{$phone}'";
-$aQuery = "DELETE FROM areaSubList WHERE phoneNumber = '{$phone}'";
+$dQuery = "DELETE FROM dailySubList WHERE phoneNumber = '{$phone}'"; //delete 1 record
+$aQuery = "DELETE FROM areaSubList WHERE phoneNumber = '{$phone}'"; //delete 1 record
 
-$dailyQuery = "INSERT INTO dailySubList (postcode, phoneNumber) VALUES ('{$postcode}', '{$phone}')";
-$areaQuery = "INSERT INTO areaSubList (phoneNumber) VALUES ('{$phone}')";
+$dailyQuery = "INSERT INTO dailySubList (postcode, phoneNumber) VALUES ('{$postcode}', '{$phone}')"; //insert new record in service 1
+$areaQuery = "INSERT INTO areaSubList (phoneNumber) VALUES ('{$phone}')"; //insert new record in service 2
 
 if (validatePhoneFormat($phone)) { //make sure the formate of phone follow 04xxxxxxxxpattern
     if ($func == 'insert') { //insert new data
         if ($checkPhoneFromArea->num_rows || $checkPhoneFromDaily->num_rows) { //make sure it's a new phone number
-            echo json_encode(array("code"=>0,"msg"=> 'Looks like you already subscribed, to update preference, please go to update page'));
-        } else {
-            switch ($serviceType) {
+            //if user already subed, we throw code 0, with error msg
+            echo json_encode(array("code"=>0,"msg"=> 'Oops! Looks like you have already subscribed with this mobile number.'));
+        } else { //it's a new number want to sub
+            switch ($serviceType) { //store record based on service type
                 case 1: //service 1 only
                     if ($db->query($dailyQuery)) {
                         echo json_encode(array("code"=>1,"msg"=>'Congratulations, You have been successfully subscribed with us!'));
-                        sendSMS($phone,"G\'day! We\'ll check risk for you and send you msg if level is high. update or unsubscribe go to");
+                        sendSMS($phone,"G\'day! We will keep you posted if risk levels increase to high or extreme. Unsub: https://tasthma.tk/unsub.html");
                     } else {
                         echo json_encode(array("code"=>0,"msg"=> 'Oops! Looks like it\'s a invalid postcode'));
                     };
                     break;
                 case 2: //service 2 only
-                    if ($db->query($areaQuery)) {
+                    if ($db->query($areaQuery)) { //if insert successful
                         echo json_encode(array("code"=>2,"msg"=>'Congratulations, You have been successfully subscribed with us!'));
-                        sendSMS($phone,"G\'day! we will send you alert in daytime when risk is high in melb. update or unsub go to");
+                        sendSMS($phone,"G\'day! We will keep you posted if risk levels increase to high or extreme.  Unsub: https://tasthma.tk/unsub.html");
                     } else {
                         echo json_encode(array("code"=>0,"msg"=> 'Oops! Looks like we had a issues, please try again'));
                     };
@@ -51,13 +64,13 @@ if (validatePhoneFormat($phone)) { //make sure the formate of phone follow 04xxx
                     if ($db->query($areaQuery)) {
                         if ($db->query($dailyQuery)) {
                             echo json_encode(array("code"=>3,"msg"=>'Congratulations, You have been successfully subscribed with us!'));
-                            sendSMS($phone,"G\'day! We'notify you any changes in melb. update or unsub, go to");
-                        } else {
+                            sendSMS($phone,"G\'day! We will keep you posted if risk levels increase to high or extreme.  Unsub: https://tasthma.tk/unsub.html");
+                        } else { //if insert into second table failed, delete inserted record.
                             $db->query($dQuery);
                             $db->query($aQuery);
                             echo json_encode(array("code"=>0,"msg"=>'Oops! we got an error storing daily'));
                         }
-                    } else {
+                    } else { //if insert first record failed, return error code and msg
                         echo json_encode(array("code"=>0,"msg"=> 'Oops! Looks like we had a issues with area'));
                     };
                     break;
@@ -65,32 +78,27 @@ if (validatePhoneFormat($phone)) { //make sure the formate of phone follow 04xxx
                     echo json_encode(array("code"=>0,"msg"=>'it\'s not a valid subscription'));
             }
         }
-    } else { //update record
-        if ($checkPhoneFromArea->num_rows || $checkPhoneFromDaily->num_rows) {
-
+    } else if ($func == 'update') { //update record
+        if ($checkPhoneFromArea->num_rows || $checkPhoneFromDaily->num_rows) { //user has subed
             switch ($serviceType) {
                 case 1: //service 1 only
-                    if($checkPostcode->num_rows) {
-
-                        $db->query($dQuery);
-                        $db->query($aQuery);
-
-                        if ($db->query($dailyQuery)) {
+                    if($checkPostcode->num_rows) { //check if the postcode provided valid
+                        $db->query($dQuery); //clean record
+                        $db->query($aQuery); //clean record
+                        if ($db->query($dailyQuery)) { //insert new
                             echo json_encode(array("code"=>4,"msg"=>'Congratulations, preference updated!'));
-                            sendSMS($phone,"Thank you! your preference has been updated!");
+                            sendSMS($phone,"Thank you! Your preferences have been updated.  Unsub: https://tasthma.tk/unsub.html");
                         } else {
                             echo json_encode(array("code"=>0,"msg"=> 'Oops! Looks like we can\'t update to one'));
                         };
                     }
                     break;
                 case 2: //service 2 only
-
                     $db->query($dQuery);
                     $db->query($aQuery);
-
-                    if ($db->query($areaQuery)) {
+                    if ($db->query($areaQuery)) { //execute to insert new record
                         echo json_encode(array("code"=>5,"msg"=>'Congratulations, preference updated!'));
-                        sendSMS($phone,"Thank you! your preference has been updated!");
+                        sendSMS($phone,"Thank you! Your preferences have been updated.  Unsub: https://tasthma.tk/unsub.html");
                     } else {
                         echo json_encode(array("code"=>0,"msg"=> 'Oops! Looks like we had a issues updating to 2'));
                     };
@@ -98,14 +106,16 @@ if (validatePhoneFormat($phone)) { //make sure the formate of phone follow 04xxx
                 case 3: //both
                     if ($checkPostcode->num_rows) {
 
-                        $db->query($dQuery);
-                        $db->query($aQuery);
+                        $db->query($dQuery); //remove all record
+                        $db->query($aQuery); //remove all record
 
-                        if ($db->query($areaQuery)) {
-                            if ($db->query($dailyQuery)) {
+                        if ($db->query($areaQuery)) { //insert into 2
+                            if ($db->query($dailyQuery)) { //insert into on if 2 successed
                                 echo json_encode(array("code"=>6,"msg" => 'Congratulations, preference updated!'));
-                                sendSMS($phone, "Thank you! your preference has been updated!");
-                            } else {
+                                sendSMS($phone, "Thank you! Your preferences have been updated.  Unsub: https://tasthma.tk/unsub.html");
+                            } else { //insert into 1 failed,
+                                $db->query($dQuery); //remove all record
+                                $db->query($aQuery);
                                 echo json_encode(array("code"=>0,"msg" => 'Oops! we got an error update daily'));
                             }
                         } else {
@@ -115,14 +125,18 @@ if (validatePhoneFormat($phone)) { //make sure the formate of phone follow 04xxx
                         };
                     }
                     break;
-                case 0: //unsub
-                    $db->query($dQuery);
-                    $db->query($aQuery);
-                    echo json_encode(array("code"=>7,"msg"=>'It\'s sad to see you go, please be safe'));
-                    break;
                 default:
                     echo json_encode(array("code"=>0,"msg"=>'it\'s not a valid update'));
             }
+        } else {
+            echo json_encode(array("code"=>0,"msg"=> 'Looks like you haven\'t subscribed yet!'));
+        }
+    } else { //unsub
+        if ($checkPhoneFromArea->num_rows || $checkPhoneFromDaily->num_rows) {
+            $db->query($dQuery);
+            $db->query($aQuery);
+            echo json_encode(array("code"=>7,"msg"=>'It\'s sad to see you go, please be safe'));
+            sendSMS($phone, "Unsub confirmed. You will no longer receive SMS alerts from us.");
         } else {
             echo json_encode(array("code"=>0,"msg"=> 'Looks like you haven\'t subscribed yet!'));
         }
